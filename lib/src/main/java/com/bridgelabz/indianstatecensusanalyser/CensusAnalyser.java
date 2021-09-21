c7f75784c750d579ec3905fb5ced9e2a80c5062d
shruthishruu05 <shruthi.m@perfios.com>
2021-09-21 21:47:13 +0530

    REFACTORED : method to load indian state code

--- lib/src/main/java/com/bridgelabz/indianstatecensusanalyser/CensusAnalyser.java
+++ lib/src/main/java/com/bridgelabz/indianstatecensusanalyser/CensusAnalyser.java
@@ -1,6 +1,9 @@
 package com.bridgelabz.indianstatecensusanalyser;
 
+import com.bridgelabz.indianstatecensusanalyser.CensusAnalyserException.ExceptionType;
 import com.google.gson.Gson;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 
 import java.io.IOException;
 import java.io.Reader;
@@ -44,5 +47,35 @@ public class CensusAnalyser
 	        }
 
 	    }
-
+	    
+	    public int loadIndianStateCode(String csvFilePath) throws CensusAnalyserException {
+	    	
+	        try {
+	        	
+	        	if(csvFilePath.contains(".txt")) {
+	    			throw new CensusAnalyserException("File must be in CSV Format", ExceptionType.CENSUS_FILE_PROBLEM);
+	    		}
+	        	
+	        	Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+	            CsvToBeanBuilder<IndianStateCodeCSV> csvToBeanBuilder = new CsvToBeanBuilder<IndianStateCodeCSV>(reader);
+	            csvToBeanBuilder.withType(IndianStateCodeCSV.class);
+	            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+	            CsvToBean<IndianStateCodeCSV> csvToBean = csvToBeanBuilder.build();
+	            Iterator<IndianStateCodeCSV> stateCodesCSVIterator = csvToBean.iterator();
+	            
+	            int numberOfEntries = 0;
+	    		while(stateCodesCSVIterator.hasNext()) {
+	    			numberOfEntries++;
+	    			IndianStateCodeCSV censusData = stateCodesCSVIterator.next();
+	    		}
+	    		return numberOfEntries;
+	        } 
+	        catch (IOException e) {
+	            throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+	        } 
+	        catch(RuntimeException e) {
+	    		throw new CensusAnalyserException("CSV File Must Have Comma As Delimiter Or Has Incorrect Header", ExceptionType.CENSUS_WRONG_DELIMITER_OR_WRONG_HEADER);
+	    	}
+	    }    
+	  	    
 }
