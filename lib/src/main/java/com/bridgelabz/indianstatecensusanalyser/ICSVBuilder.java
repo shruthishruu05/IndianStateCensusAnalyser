3c788335bf8c500a704c9a491e3a0c57ed8d619b
shruthishruu05 <shruthi.m@perfios.com>
2021-09-21 20:24:25 +0530

    ADDED : interface containg iterators and list for csv file

--- lib/src/main/java/com/bridgelabz/indianstatecensusanalyser/ICSVBuilder.java
+++ lib/src/main/java/com/bridgelabz/indianstatecensusanalyser/ICSVBuilder.java
@@ -4,7 +4,8 @@ package com.bridgelabz.indianstatecensusanalyser;
 	import java.util.Iterator;
 	import java.util.List;
 
-	public interface ICSVBuilder {
+	public interface ICSVBuilder 
+	{
 	    public  <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws CSVBuilderException;
 	    public  <E> List<E> getCSVFileList(Reader reader, Class<E> csvClass) throws CSVBuilderException;
 	}
